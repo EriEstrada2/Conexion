ac78c4b86580685a23f297952337047caf3be7fa
Alexis Rivera <david.rivera@espoch.edu.ec>
2023-12-01 12:28:26 -0500

    correccion de archivos

--- test.js
+++ test.js
@@ -12,39 +12,30 @@ db.sync({ force: true })
     console.log('Modelos sincronizados correctamente.');
 
     // Crear registros de ejemplo
-    return Emisor.create({
-      NombreComercial: 'Mi Compañía',
-      RazonSocial: 'Compañía S.A.',
-      DireccionMatriz: 'Dirección Matriz',
-      DireccionEstablecimiento: 'Dirección Establecimiento',
+// Crear registros de ejemplo
+return Promise.all([
+  Adquirente.create({
+    Identificacion: '123456789', // Asegúrate de proporcionar un valor válido
+    Nombre: 'Juan',
+    Apellido: 'Perez',
+    Firma: 'Firma1',
+    FechaCompra: new Date(),
+  }),
+  Imprenta.create({
+    NombreDueno: 'Dueño1',
+    NombreImprenta: 'Imprenta1',
+    RUC: '1234567890123',
+    NumeroAutorizacion: 'Autorizacion1',
+    Destinatario: 'Destinatario1',
+  }),
+  // Puedes continuar con las otras tablas según sea necesario
+]);
+
+  })
+  .then((results) => {
+    results.forEach((result, index) => {
+      console.log(`Registro creado en ${result.constructor.name}:`, result.toJSON());
     });
-  })
-  .then((emisor) => {
-    console.log('Emisor creado:', emisor.toJSON());
-
-    // Actualizar un campo
-    return emisor.update({ NombreComercial: 'Nuevo Nombre Comercial' });
-  })
-  .then((emisor) => {
-    console.log('Emisor actualizado:', emisor.toJSON());
-
-    // Buscar todos los emisores
-    return Emisor.findAll();
-  })
-  .then((emisores) => {
-    console.log('Todos los emisores:', emisores.map((e) => e.toJSON()));
-
-    // Eliminar un emisor
-    return Emisor.destroy({ where: { id: 1 } });
-  })
-  .then(() => {
-    console.log('Emisor eliminado.');
-
-    // Consultar nuevamente todos los emisores después de eliminar uno
-    return Emisor.findAll();
-  })
-  .then((emisores) => {
-    console.log('Todos los emisores después de eliminar uno:', emisores.map((e) => e.toJSON()));
   })
   .catch((error) => {
     console.error('Error:', error);
